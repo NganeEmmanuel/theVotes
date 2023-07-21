263cd432e6c8f799c56a1726990efe82efd2ea78
Ngane Emmanuel <Ngane Emmanuel@DESKTOP-A2QJHIJ>
2023-07-20 19:02:21 -0700

    Initial check in theVotes

---
+++ theVotes/src/main/java/com/thevotes/TheVotesApplication.java
@@ -0,0 +1,13 @@
+package com.thevotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheVotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheVotesApplication.class, args);
+	}
+
+}
---
+++ theVotes/src/test/java/com/thevotes/TheVotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thevotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheVotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
